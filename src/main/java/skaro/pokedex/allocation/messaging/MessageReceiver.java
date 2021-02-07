012b231aae291d1074a8e05909144a2bd0f0fd4f
unknown <bencbchurchill@gmail.com>
2021-02-07 14:26:40 -0700

    Changed business logic to receive a pojo instead of a raw string from the worker queue

--- src/main/java/skaro/pokedex/allocation/messaging/MessageReceiver.java
+++ src/main/java/skaro/pokedex/allocation/messaging/MessageReceiver.java
@@ -6,14 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import skaro.pokedex.sdk.messaging.DiscordTextEventMessage;
+
 @Component
 public class MessageReceiver {
 
 	private final static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	public static final String RECIEVE_METHOD_NAME = "receive";
 	
-    public void receive(String message) {
-		LOG.info("Received message: {}", message);
+    public void receive(DiscordTextEventMessage message) {
+		LOG.info("{} says '{}' from channel {} ", message.getAuthorId(), message.getContent(), message.getChannelId());
 	}
 	
 }
--- src/main/java/skaro/pokedex/allocation/messaging/MessagingConfiguration.java
+++ src/main/java/skaro/pokedex/allocation/messaging/MessagingConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import skaro.pokedex.sdk.GatewayMessagingConfiguration;
+import skaro.pokedex.sdk.messaging.GatewayMessagingConfiguration;
 
 @Configuration
 @Import(GatewayMessagingConfiguration.class)
